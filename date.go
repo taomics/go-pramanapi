b3a53a7c0825101c7e707e582ff9dc786adefab5
Shintaro Abe <dictav@gmail.com>
2025-10-27 17:16:20 +0900

    feat: generate v0.20251027.0 (#9)

--- date.go
+++ date.go
@@ -33,6 +33,10 @@ func (d *Date) DaysFrom(t time.Time) int {
 // Time converts a Date to a time.Time (at midnight UTC).
 // Usually, Date is initialized using user input, so this function returns an error if the Date is invalid.
 func (d *Date) Time() (time.Time, error) {
+	if d == nil {
+		return time.Time{}, fmt.Errorf("date is nil")
+	}
+
 	return time.Parse("2006-01-02", fmt.Sprintf("%04d-%02d-%02d", d.Year, d.Month, d.Day))
 }
 
